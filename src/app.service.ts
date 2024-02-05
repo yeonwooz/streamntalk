dafecc7833993558126c6f01f6c3620bc58a8892
yeonwooz <woosean999@gmail.com>
2024-02-05 21:20:16 +0900

    feat: use ConfigService

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
+    // use configService to get env variables
     return 'Hello World!';
   }
 }
