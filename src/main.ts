b68e6ac9cb4e8372e05af1bd083597f9f44f1266
yeonwooz <woosean999@gmail.com>
2024-02-11 19:54:07 +0900

    feat(docs): add swagger document

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
-declare const module: any;
-
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Streamntalk API')
+    .setDescription('Streamntalk API 문서')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 
   if (module.hot) {
     module.hot.accept();
