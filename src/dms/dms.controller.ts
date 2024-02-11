8f5f47a78e77cf13a5d999cac63426d0e9a361ab
yeonwooz <woosean999@gmail.com>
2024-02-11 17:25:09 +0900

    fix(dm-controller): shorten parameters of getChats

--- src/dms/dms.controller.ts
+++ src/dms/dms.controller.ts
@@ -3,7 +3,8 @@ import { Controller, Get, Post, Query } from '@nestjs/common';
 @Controller('api/workspaces/:url/dms')
 export class DmsController {
   @Get(':id/chats')
-  getChats(@Query('perPage') perPage, @Query('page') page) {
+  getChats(@Query() query) {
+    const { perPage, page } = query;
     console.log(perPage, page);
   }
 
