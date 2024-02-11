e82b343fa640c08fbb8efc382b18597ef0fc029b
yeonwooz <woosean999@gmail.com>
2024-02-11 20:30:32 +0900

    feat(user-controller): add meta

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,24 +1,30 @@
 import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
 
 @Controller('api/users')
 export class UsersController {
   constructor(private userService: UsersService) {}
 
+  @ApiOperation({ summary: '내 정보 조회' })
   @Get()
   getUsers(@Req() req) {
     return req.user;
   }
 
+  @ApiOperation({ summary: '회원가입' })
+  @ApiBody({ type: JoinRequestDto })
   @Post()
   postUsers(@Body() data: JoinRequestDto) {
     this.userService.createUser(data);
   }
 
+  @ApiOperation({ summary: '로그인' })
   @Post('login')
   logIn() {}
 
+  @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
   logout(@Req() req, @Res() res) {
     // express 문법을 사용
