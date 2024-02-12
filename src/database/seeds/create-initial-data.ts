845738a16b8148d9554803286fbc9600ddee5493
yeonwooz <woosean999@gmail.com>
2024-02-13 00:14:16 +0900

    feat(db): add seed

---
+++ src/database/seeds/create-initial-data.ts
@@ -0,0 +1,29 @@
+import { Seeder, SeederFactoryManager } from 'typeorm-extension';
+import { DataSource } from 'typeorm';
+import { Workspaces } from '../../entities/Workspaces';
+import { Channels } from '../../entities/Channels';
+
+export default class UserSeeder implements Seeder {
+  public async run(
+    dataSource: DataSource,
+    factoryManager: SeederFactoryManager,
+  ): Promise<any> {
+    const workspacesRepository = dataSource.getRepository(Workspaces);
+    await workspacesRepository.insert([
+      {
+        id: 1,
+        name: 'StreamNTalk',
+        url: 'streamntalk',
+      },
+    ]);
+    const channelsRepository = dataSource.getRepository(Channels);
+    await channelsRepository.insert([
+      {
+        id: 1,
+        name: '일반',
+        WorkspaceId: 1,
+        private: false,
+      },
+    ]);
+  }
+}
