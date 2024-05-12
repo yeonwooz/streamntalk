622a522a8e8dc7d606ecbbd142226817d08f05aa
yeonwooz <woosean999@gmail.com>
2024-05-12 22:58:19 +0900

    feat(users.service.ts): join api

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { JoinRequestDto } from './dto/join.request.dto';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from 'src/entities/Users';
+import { Repository } from 'typeorm';
+import bcrypt from 'bcrypt';
 @Injectable()
 export class UsersService {
-  createUser(data: JoinRequestDto) {}
+  constructor(
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>,
+  ) {}
+
+  getUser() {}
+
+  async createUser(data: JoinRequestDto) {
+    const { email, password, nickname } = data;
+    if (!email) {
+      throw new HttpException('No email', 400);
+    }
+    if (!nickname) {
+      throw new HttpException('No nickname', 400);
+    }
+    if (!password) {
+      throw new HttpException('No password', 400);
+    }
+    const user = await this.usersRepository.findOne({ where: { email } });
+    if (user) {
+      throw new HttpException('User already exists', 409);
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    await this.usersRepository.save({
+      email,
+      nickname,
+      password: hashedPassword,
+    });
+  }
 }
